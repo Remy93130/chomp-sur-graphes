2d253d7ba50ee4158e8034142daf1375fc94f3b0
LeoChardon <45102426+LeoChardon@users.noreply.github.com>
2019-01-23 11:06:22 +0100

    MAJ

--- src/view/GUI.py
+++ src/view/GUI.py
@@ -3,6 +3,7 @@
 
 from tkinter import *
 from tkinter import font as tkFont
+from aggdraw import *
 
 class GUI:
 	def __init__(self, width,height):
@@ -59,7 +60,7 @@ class GUI:
 			color = 'lightblue'
 		else:
 			color = 'white'
-		self.canvas.create_oval(node.coord[0] +self.width/5 -27, self.graph_height+self.height/6 - node.coord[1]-18, node.coord[0] +self.width/5 +27, self.graph_height+self.height/6 - node.coord[1]+18,tag=("_"+node.id_node+"_","graph"),fill=color)
+		self.canvas.create_oval(node.coord[0] +self.width/5 -27, self.graph_height+self.height/6 - node.coord[1]-18, node.coord[0] +self.width/5 +27, self.graph_height+self.height/6 - node.coord[1]+18,tag=("_"+node.id_node+"_","graph"),fill=color,width=1.5)
 		self.canvas.create_text(node.coord[0] +self.width/5 , self.graph_height+self.height/6 - node.coord[1], text=node.id_node,tag=("_"+node.id_node+"_","graph"))
 		
 	def delNode(self,node):
@@ -68,12 +69,12 @@ class GUI:
 	def drawTimer(self):
 		return self.canvas.create_text(self.height*0.46,self.height*0.083,font=self.timerFont)
 	
-	def drawTimeRest(self):
-		self.canvas.create_text(self.height*0.46,self.height*0.0555,font=self.timerFont,text="Temps restant : ")
+	def drawTimeRest(self,player):
+		self.canvas.create_text(self.height*0.46,self.height*0.0555,font=self.timerFont,text="Temps restant au joueur "+str(player)+" : ",tag="graph")
 	
 	def drawArrow(self,arrow):
 		if arrow.id_arrow[0] in self.nodes and arrow.id_arrow[1] in self.nodes:
-				self.canvas.create_line(float(arrow.points_line[0])+self.width/5, arrow.points_line[1]-self.height+self.graph_height+self.height/6, float(arrow.points_line[2])+self.width/5, arrow.points_line[3]-self.height+self.graph_height+self.height/6,tag="graph")
+				self.canvas.create_line(float(arrow.points_line[0])+self.width/5, arrow.points_line[1]-self.height+self.graph_height+self.height/6, float(arrow.points_line[2])+self.width/5, arrow.points_line[3]-self.height+self.graph_height+self.height/6,tag="graph",width=2.5)
 				self.canvas.create_polygon(arrow.points_sting,tag="graph")
 		
 	def drawArrows(self):
@@ -82,6 +83,9 @@ class GUI:
 	def drawNodes(self):
 		for node in self.nodes.values():
 			self.drawNode(node)
+			
+	def hideTimer(self,tag):
+		self.canvas.create_rectangle(self.height*0.411, self.height*0.066, self.height*0.511, self.height*0.094,outline='white',fill='white',width=0,tag=tag)
 	
 	def drawMenu(self):
 		titleFont = tkFont.Font(family='Arial', size=int(self.height*0.0442), weight='bold')
@@ -93,11 +97,20 @@ class GUI:
 		self.canvas.create_rectangle(self.height*0.393, self.height*0.75, self.height*0.607, self.height*0.857,outline='black',fill='white',activefill='grey',width=1,tag='about')
 		self.canvas.create_rectangle(self.height*0.642, self.height*0.235, self.height*0.771, self.height*0.3,outline='black',fill='white',activefill='grey',width=1,tag='rules')
 		self.canvas.create_text(self.height*0.5, self.height*0.271, text="1 Joueur",tag="1joueur",font=ariaRespon)
-		self.canvas.create_text(self.height*0.5, self.height*0.45, text="2 Joueur",tag="2joueur",font=ariaRespon)
-		self.canvas.create_text(self.height*0.5, self.height*0.628, text="Option",tag="option",font=ariaRespon)
+		self.canvas.create_text(self.height*0.5, self.height*0.45, text="2 Joueurs",tag="2joueur",font=ariaRespon)
+		self.canvas.create_text(self.height*0.5, self.height*0.628, text="Options",tag="option",font=ariaRespon)
 		self.canvas.create_text(self.height*0.5, self.height*0.8, text="À propos",tag="about",font=ariaRespon)
 		self.canvas.create_text(self.height*0.7, self.height*0.271, text="règles",tag="rules",font=ariaRespon)
-	
+		
+	def drawRules(self):
+		titleFont = tkFont.Font(family='Arial', size=int(self.height*0.0442), weight='bold')
+		describeFont = tkFont.Font(family='Arial', size=int(self.height*0.0116))
+		self.canvas.create_text(self.height*0.50, self.height*0.12, text="Les règles du Chomp",tag="rulesTitle",font=titleFont)
+		self.canvas.create_text(self.height*0.222, self.height*0.227, text="Le but du jeu est de forcer votre adversaire à cliquer sur le noeud empoisonné.\n\nLe jeu se joue tour par tour, si vous cliquez sur un noeud alors celui-ci \n et tout les noeuds reliés à lui disparaitront.\n\n Le joueur ayant fait disparaitre le noeud empoisonné perd la partie",tag="rulesDescribe",anchor="nw")
+		self.canvas.create_rectangle(self.height*0.016,self.height*0.016,self.height*0.116,self.height*0.072,tag="returnM",fill="white",activefill="grey")
+		self.canvas.create_text(self.height*0.061,self.height*0.044,text="Menu",activefill="grey",font=describeFont)
+		
+		
 	def launchGUI(self):
 		self.canvas.pack()
 		self.screen.mainloop()
