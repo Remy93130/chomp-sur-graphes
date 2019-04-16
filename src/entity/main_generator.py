a64787733021b073b707364186d9d3646fb4c059
Remy93130 <remy93130@gmail.com>
2019-04-16 23:55:37 +0200

    repost generator
    Maybe i need to push it again cos' i fucked up idk 🤫

---
+++ src/entity/main_generator.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*
+"""[summary]
+[description]
+"""
+
+GRAPH_FOLDER = "../ressources/graphes/"
+
+import os
+import pickle
+
+def save_graph(new_name="", graph_name="Digraph.gv"):
+    if not new_name:
+        new_name = __get_graph_name()
+    os.rename("./" + graph_name, GRAPH_FOLDER + new_name)
+    os.remove(graph_name)
+
+def __get_graph_name():
+    """Function for generate a graph name if no one is given
+    or the name already exist the name will be the number
+    of the graph for example if they are already 3 graph
+    created then the name will be 4
+    Returns:
+        str -- The new graph name
+    """
+    all_graphs = os.listdir(GRAPH_FOLDER)
+    return str(len(all_graphs) + 1)
+
+def serialize_graph(graph_path):
+    """Parse the graph and serialize this one
+    for avoid parse it every time we want use
+    this one.    
+    Arguments:
+        graph_path {str} -- The relative path from the graph
+    """
+    pass
+
+
+"""
+pickle.dump(Object, open(filename, 'wb'))
+object = pickle.load(open(filename, 'rb'))
+"""
